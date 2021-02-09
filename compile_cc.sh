0b63ddc459c0d61a7740abde8877af281f63f6d5
Pafaul <paulmihailov2013@gmail.com>
2021-02-09 15:58:17 +0300

    Update of build script

--- compile_cc.sh
+++ compile_cc.sh
@@ -4,8 +4,8 @@ SC_DIR=$1
 TARGET_DIR=$2
 COMPILE_SH=$(cd $(dirname $0) && pwd)/compile_sc.sh
 
-[ ! -d ${SC_DIR} ] && echo "SC dir does not exist" && exit 1
-[ ! -d ${TARGET_DIR} ] && mkdir -p ${TARGET_DIR}
+[ -z ${SC_DIR} ] && echo "SC dir does not exist" && exit 1
+[ -z ${TARGET_DIR} ] && mkdir -p ${TARGET_DIR}
 
 bash ${COMPILE_SH} ${SC_DIR} TONTokenWallet $TARGET_DIR
 bash ${COMPILE_SH} ${SC_DIR} RootTokenContract $TARGET_DIR
