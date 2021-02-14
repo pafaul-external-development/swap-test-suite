ee5e623cc085a2bde73cb05452a03993db3f2ac8
Paul Mikhaylov <paulmihailov2013@gmail.com>
2021-02-14 18:15:22 +0300

    Update due to smart contracts repo layout change

--- compile_cc.sh
+++ compile_cc.sh
@@ -7,7 +7,10 @@ COMPILE_SH=$(cd $(dirname $0) && pwd)/compile_sc.sh
 [ -z ${SC_DIR} ] && echo "SC dir does not exist" && exit 1
 [ -z ${TARGET_DIR} ] && mkdir -p ${TARGET_DIR}
 
-bash ${COMPILE_SH} ${SC_DIR} TONTokenWallet $TARGET_DIR
-bash ${COMPILE_SH} ${SC_DIR} RootTokenContract $TARGET_DIR
-bash ${COMPILE_SH} ${SC_DIR}/tests CallbackTestContract $TARGET_DIR
-bash ${COMPILE_SH} ${SC_DIR}/tests DeployEmptyWalletFor $TARGET_DIR
+bash ${COMPILE_SH} ${SC_DIR}/RIP-3 TONTokenWallet $TARGET_DIR
+bash ${COMPILE_SH} ${SC_DIR}/RIP-3 RootTokenContract $TARGET_DIR
+bash ${COMPILE_SH} ${SC_DIR}/RIP-3/tests CallbackTestContract $TARGET_DIR
+bash ${COMPILE_SH} ${SC_DIR}/RIP-3/tests DeployEmptyWalletFor $TARGET_DIR
+
+bash ${COMPILE_SH} ${SC_DIR}/SwapPair RootSwapPairContract $TARGET_DIR
+bash ${COMPILE_SH} ${SC_DIR}/SwapPair SwapPairContract $TARGET_DIR
