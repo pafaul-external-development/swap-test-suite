7a1d347142ea1696be7845204813afc106d23781
Paul Mikhaylov <paulmihailov2013@gmail.com>
2021-03-14 14:34:25 +0300

    upd of submodule

--- compile_cc.sh
+++ compile_cc.sh
@@ -7,10 +7,10 @@ COMPILE_SH=$(cd $(dirname $0) && pwd)/compile_sc.sh
 [ -z ${SC_DIR} ] && echo "SC dir does not exist" && exit 1
 [ -z ${TARGET_DIR} ] && mkdir -p ${TARGET_DIR}
 
-bash ${COMPILE_SH} ${SC_DIR}/RIP-3 TONTokenWallet $TARGET_DIR
-bash ${COMPILE_SH} ${SC_DIR}/RIP-3 RootTokenContract $TARGET_DIR
-bash ${COMPILE_SH} ${SC_DIR}/RIP-3/tests CallbackTestContract $TARGET_DIR
-bash ${COMPILE_SH} ${SC_DIR}/RIP-3/tests DeployEmptyWalletFor $TARGET_DIR
+bash ${COMPILE_SH} ${SC_DIR}/TIP-3 TONTokenWallet $TARGET_DIR
+bash ${COMPILE_SH} ${SC_DIR}/TIP-3 RootTokenContract $TARGET_DIR
+bash ${COMPILE_SH} ${SC_DIR}/TIP-3/tests CallbackTestContract $TARGET_DIR
+bash ${COMPILE_SH} ${SC_DIR}/TIP-3/tests DeployEmptyWalletFor $TARGET_DIR
 
 bash ${COMPILE_SH} ${SC_DIR}/additional/ GiverContract ${TARGET_DIR}
 bash ${COMPILE_SH} ${SC_DIR}/additional/ DevNetGiver ${TARGET_DIR}
